181b747077be29b486db66b51109b62bb6dd7c25
viana <129645315+viana-alipanahifard@users.noreply.github.com>
2025-08-23 22:00:09 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 
 def send_otp_code(phone_number,code):
     try:
-        api=KavenegarAPI('316565596B4A47384A68322F4A3069506A45754B3745655146495842434A496E41657A6151774E446C4')
+        api=KavenegarAPI('*')
         params={
             'sender':'2000660110',
             'receptor':phone_number,
